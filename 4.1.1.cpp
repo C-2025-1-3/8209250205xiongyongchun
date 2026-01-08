ef9915091dc8633c0939c952eae4601febc8b8ad
xyc2008 <2906294495@qq.com>
2026-01-08 10:06:34 +0800

    Change loop to index-based for distinct number output
    Updated the loop to iterate over the nums array using its index instead of a range-based for loop.

--- 4.1.1.cpp
+++ 4.1.1.cpp
@@ -9,18 +9,19 @@ int main() {
         cin >> nums[i];
     }
     cout << "The distinct numbers are:";
-    for (int num : nums) {  
-        if (num >= 0 && num < MAX_NUM) {  // 确保输入在标记数组范围内
-            if (!exists[num]) {           // 如果未出现过
-                cout << num << " ";
-                exists[num] = true;       // 标记为已出现
-            }
-        }
+    for (int i = 0; i < 10; i++) {  
+       if (nums[i] >= 0 && nums[i] < MAX_NUM) {  // 确保输入在标记数组范围内
+        if (!exists[nums[i]]) {           // 如果未出现过
+            cout << nums[i] << " ";
+            exists[nums[i]] = true;       // 标记为已出现
+                }
+    }
         else {
-            cout << "\n警告：输入的数" << num << "超出范围（0~" << MAX_NUM - 1 << "），已忽略。" << endl;
+            cout << "\n警告：输入的数" << nums[i] << "超出范围（0~" << MAX_NUM - 1 << "），已忽略。" << endl;
         }
     }
 
     return 0;
 
 }
+
